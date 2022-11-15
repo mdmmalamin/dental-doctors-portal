af03e1c745f1272c3753545dbcdc8f634090f1f9
mdmmalamin <mdmmalamin@gmail.com>
2022-11-16 00:03:16 +0600

    login form created & firebase auth setup

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,25 +1,40 @@
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
-    const { register, handleSubmit } = useForm()
-    const [ data, setData ] = useState('');
+    const { register, handleSubmit } = useForm();
+
+    const handleLogin = data => {
+        console.log(data);
+    }
 
     return (
-        <section className='h-[80vh] flex justify-center items-center'>
-            <div>
-                <h2>This is Login</h2>
-                <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
-                    <input {...register("firstName")} placeholder="First name" />
-                    <select {...register("category", { required: true })}>
-                        <option value="">Select...</option>
-                        <option value="A">Option A</option>
-                        <option value="B">Option B</option>
-                    </select>
-                    <textarea {...register("aboutYou")} placeholder="About you" />
-                    <p>{data}</p>
-                    <input type="submit" />
+        <section className='h-[80vh] md:w-1/2 mx-auto flex justify-center items-center'>
+            <div className='shadow-md rounded-xl p-7 w-96'>
+                <h2 className='text-xl text-center mb-8'>Login</h2>
+                <form onSubmit={handleSubmit(handleLogin)}>
+                    
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label p-1">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input type="email" {...register("email")} className="input input-bordered w-full" />
+                    </div>
+                    <div className="form-control w-full max-w-xs my-2.5">
+                        <label className="label p-1">
+                            <span className="label-text">Password</span>
+                        </label>
+                        <input type="password" {...register("password")} className="input input-bordered w-full" />
+                        <label className="label p-1">
+                            <span className="text-xs">Forgot Password?</span>
+                        </label>
+                    </div>
+                    <input className='btn btn-accent w-full' value="LOGIN" type="submit" />
                 </form>
+                <p className='text-xs mt-2.5 text-center'>New to Doctors Portal? <Link to='/' className='text-secondary'>Create new account</Link></p>
+                <div className="divider mt-4 mb-6">OR</div>
+                <button className='btn bg-white text-black w-full'>CONTINUE WITH GOOGLE</button>
             </div>
         </section>
     );
