2b20b38ff75a3b7ca323d63fddfad601dba14376
Imon ahmed liku <109808505+likupiku@users.noreply.github.com>
2024-02-19 14:08:10 +0600

    past days desabled in appointment calender

--- src/Pages/Appointment/AppointmentBanner/AppointmentBanner.js
+++ src/Pages/Appointment/AppointmentBanner/AppointmentBanner.js
@@ -1,28 +1,34 @@
-import React from 'react';
-import chair from '../../../assets/images/chair.png';
-import bg from '../../../assets/images/bg.png';
-import { DayPicker } from 'react-day-picker';
+import React from "react";
+import chair from "../../../assets/images/chair.png";
+import bg from "../../../assets/images/bg.png";
+import { DayPicker } from "react-day-picker";
 
 const AppointmentBanner = ({ selectedDate, setSelectedDate }) => {
-
-
-    return (
-        <header className='bg-contain bg-no-repeat my-10 lg:py-48' style={{backgroundImage: `url(${bg})`}}>
-            <div className="hero">
-                <div className="hero-content flex-col lg:flex-row-reverse justify-around gap-6">
-                    <img src={chair} className="rounded-lg lg:w-1/2 shadow-lg" alt="dentist chair" />
-                    <div className='shadow-lg rounded-xl bg-white'>
-                        <DayPicker 
-                            // disabled={new Date()}
-                            mode='single'
-                            selected={selectedDate}
-                            onSelect={setSelectedDate}
-                        />
-                    </div>
-                </div>
-            </div>
-        </header>
-    );
+  return (
+    <header
+      className="bg-contain bg-no-repeat my-10 lg:py-48"
+      style={{ backgroundImage: `url(${bg})` }}
+    >
+      <div className="hero">
+        <div className="hero-content flex-col lg:flex-row-reverse justify-around gap-6">
+          <img
+            src={chair}
+            className="rounded-lg lg:w-1/2 shadow-lg"
+            alt="dentist chair"
+          />
+          <div className="shadow-lg rounded-xl bg-white">
+            <DayPicker
+              // disabled={new Date()}
+              mode="single"
+              selected={selectedDate}
+              onSelect={setSelectedDate}
+              disabled={{ before: new Date() }}
+            />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default AppointmentBanner;
