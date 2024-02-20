3e6607cf7324ea31e67e756d785d3f26ca5836be
emonahmed <emon.ahmedliku@gmail.com>
2024-02-20 14:49:05 +0600

    otp half done

--- src/Pages/Appointment/BookingModal/BookingModal.js
+++ src/Pages/Appointment/BookingModal/BookingModal.js
@@ -1,87 +1,147 @@
-import { format } from 'date-fns';
-import React, { useContext } from 'react';
-import toast from 'react-hot-toast';
-import { AuthContext } from '../../../contexts/AuthProvider';
+import { format } from "date-fns";
+import React, { useContext, useState } from "react";
+import toast from "react-hot-toast";
+import { AuthContext } from "../../../contexts/AuthProvider";
+import PhoneInput from "react-phone-input-2";
+import OTPInput from "otp-input-react";
+import "./BookingModal..css";
 
 const BookingModal = ({ treatment, setTreatment, selectedDate, refetch }) => {
-    // treatment is just another name of appointmentOption
-    const { name, slots, price } = treatment; 
-    // console.log(treatment);
-    const date = format(selectedDate, 'PP');
-    const { user } = useContext(AuthContext);
-    // console.log(user);
-    const today =format(new Date(), "PP");
+  // otp related
+  const [otp, setOtp] = useState("");
+  // treatment is just another name of appointmentOption
+  const { name, slots, price } = treatment;
+  // console.log(treatment);
+  const date = format(selectedDate, "PP");
+  const { user } = useContext(AuthContext);
+  // console.log(user);
+  const today = format(new Date(), "PP");
 
-    const handleBooking = event => {
-        event.preventDefault();
-        const form = event.target;
-        const patient_name = form.name.value;
-        const slot = form.slot.value;
-        const email = form.email.value;
-        const phone = form.phone.value;
+  const handleBooking = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const patient_name = form.name.value;
+    const slot = form.slot.value;
+    const email = form.email.value;
+    const phone = form.phone.value;
+    const otp = form.otp.value;
 
-        const booking = {
-            bookingDate: today,
-            appointmentDate: date,
-            treatment: name,
-            patient: patient_name,
-            slot,
-            email,
-            phone,
-            price
+    const booking = {
+      bookingDate: today,
+      appointmentDate: date,
+      treatment: name,
+      patient: patient_name,
+      slot,
+      email,
+      phone,
+      price,
+      otp,
+    };
+    // TODO: send data to the server
+    // and once data is saved then close the modal display success toast
+    console.log(booking);
+
+    fetch("https://doctors-portal-server-kappa-sooty.vercel.app/bookings", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(booking),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.acknowledged) {
+          setTreatment(null);
+          toast.success("Booking confirmed");
+
+          refetch();
+        } else {
+          toast.error(data.message);
         }
-        // TODO: send data to the server
-        // and once data is saved then close the modal display success toast
-        console.log(booking);
+      });
+  };
 
-        fetch('https://doctors-portal-server-kappa-sooty.vercel.app/bookings', {
-            method: 'POST',
-            headers: {
-                'content-type': 'application/json'
-            },
-            body: JSON.stringify(booking)
-        })
-        .then(res => res.json())
-        .then(data => {
-            console.log(data);
-            if(data.acknowledged){
-                setTreatment(null);
-                toast.success('Booking confirmed');
-                refetch();
-            }
-            else{
-                toast.error(data.message)
-            }
-        })
-
-    }
-
-    return (
-        <>
-            <input type="checkbox" id="booking-modal" className="modal-toggle" />
-            <div className="modal">
-                <div className="modal-box relative">
-                    <label htmlFor="booking-modal" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
-                    <h3 className="text-lg font-bold mb-12">{name}</h3>
-                    <form onSubmit={handleBooking} className='grid grid-cols-1 gap-3 md:gap-6'>
-                        <input type="text" disabled value={date} className="input input-bordered w-full" />
-                        <select name="slot" className="select select-bordered w-full">
-                            {
-                                slots.map((slot, idx) => <option
-                                    key={idx}
-                                    value={slot}
-                                >{slot}</option>)
-                            }
-                        </select>
-                        <input name='name' type="text" readOnly defaultValue={user?.displayName} placeholder="Your Name" className="input input-bordered w-full" />
-                        <input name='email' type="email" readOnly defaultValue={user?.email} placeholder="Email Address" className="input input-bordered w-full" />
-                        <input name='phone' type="Phone" defaultValue={user?.phoneNumber} placeholder="Phone Number" className="input input-bordered w-full" />
-                        <input type="submit" value="Submit" className='btn btn-accent w-full' />
-                    </form>
-                </div>
+  return (
+    <>
+      <input type="checkbox" id="booking-modal" className="modal-toggle" />
+      <div className="modal">
+        <div className="modal-box relative">
+          <label
+            htmlFor="booking-modal"
+            className="btn btn-sm btn-circle absolute right-2 top-2"
+          >
+            ✕
+          </label>
+          <h3 className="text-lg font-bold mb-12">{name}</h3>
+          <form
+            onSubmit={handleBooking}
+            className="grid grid-cols-1 gap-3 md:gap-6"
+          >
+            <input
+              type="text"
+              disabled
+              value={date}
+              className="input input-bordered w-full"
+            />
+            <select name="slot" className="select select-bordered w-full">
+              {slots.map((slot, idx) => (
+                <option key={idx} value={slot}>
+                  {slot}
+                </option>
+              ))}
+            </select>
+            <input
+              name="name"
+              type="text"
+              readOnly={user?.displayName}
+              defaultValue={user?.displayName}
+              placeholder="Your Name"
+              className="input input-bordered w-full"
+            />
+            <input
+              name="email"
+              type="email"
+              readOnly={user?.email}
+              defaultValue={user?.email}
+              placeholder="Email Address"
+              className="input input-bordered w-full"
+            />
+            {/* <input
+              name="phone"
+              type="Phone"
+              defaultValue={user?.phoneNumber}
+              placeholder="Phone Number"
+              className="input input-bordered w-full"
+            /> */}
+            {/* <PhoneInput
+              country={"us"}
+              value={this.state.phone}
+              onChange={(phone) => this.setState({ phone })}
+            /> */}
+            <div className="bg-gray-600/30 py-2">
+              <h1 className="text-center font-bold pb-2">Write Your OTP</h1>
+              <OTPInput
+                name="otp"
+                value={otp}
+                onChange={setOtp}
+                className="flex justify-center items-center otp-container text-black"
+                OTPLength={6}
+                otpType="number"
+                autoFocus
+                disabled={false}
+              ></OTPInput>
             </div>
-        </>
-    );
+            <input
+              type="submit"
+              value="Submit"
+              className="btn btn-accent w-full"
+            />
+          </form>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default BookingModal;
--- src/contexts/AuthProvider.js
+++ src/contexts/AuthProvider.js
@@ -1,57 +1,69 @@
-import React, { createContext, useEffect, useState } from 'react';
-import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut, updateProfile } from 'firebase/auth';
-import app from '../firebase/firebase.config';
+import React, { createContext, useEffect, useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+import app from "../firebase/firebase.config";
 
 export const AuthContext = createContext();
 const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
-    const [ user, setUser ] = useState(null);
-    const [ loading, setLoading ] = useState(true);
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-// SingUp
-    const createUser = (email, password) => {
-        setLoading(true);
-        return createUserWithEmailAndPassword(auth, email, password);
-    }
-// SignIn
-    const signIn = (email, password) => {
-        setLoading(true);
-        return signInWithEmailAndPassword(auth, email, password);
-    }
-// Update User
-    const updateUser = (userInfo) => {
-        return updateProfile(auth.currentUser, userInfo);
-    }
-// Observing
-    useEffect( () => {
-        const unsubscribe = onAuthStateChanged(auth, currentUser => {
-            console.log('user observing');
-            setUser(currentUser);
-            
-            setLoading(false);
-        });
-        return () => unsubscribe();
-    }, []);
-// Sign Out
-    const logOut = () => {
-        setLoading(true);
-        return signOut(auth);
-    }
+  // phone number sign in
+  const signInWithPhoneNumber = (phoneNumber) => {
+    setLoading(true);
+    return signInWithPhoneNumber(auth, phoneNumber);
+  };
 
-    const authInfo = {
-        createUser,
-        signIn,
-        updateUser,
-        user,
-        logOut,
-        loading
-    }
-    return (
-        <AuthContext.Provider value={authInfo}>
-            { children }
-        </AuthContext.Provider>
-    );
+  // SingUp
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+  // SignIn
+  const signIn = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+  // Update User
+  const updateUser = (userInfo) => {
+    return updateProfile(auth.currentUser, userInfo);
+  };
+  // Observing
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      console.log("user observing");
+      setUser(currentUser);
+
+      setLoading(false);
+    });
+    return () => unsubscribe();
+  }, []);
+  // Sign Out
+  const logOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+
+  const authInfo = {
+    createUser,
+    signIn,
+    updateUser,
+    user,
+    logOut,
+    loading,
+    signInWithPhoneNumber,
+  };
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
 };
 
 export default AuthProvider;
